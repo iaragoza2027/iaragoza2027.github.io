2ba6a897e902a81dda9ca9f36ef26d9bdfb61ce3
Ian Gabriel <143575114+iaragoza2027@users.noreply.github.com>
2024-05-17 10:46:43 +0800

    Fixed ordering and implemented checking out.
    (script.js)
    - Added inputCart().
    - Tweaked other functions.
    
    (order.html)
    - Assigned correct input tag names.
    - Added input tag for cartList.
    
    (index.html)
    - Reassigned #cart-total.

--- G94QSA/script.js
+++ G94QSA/script.js
@@ -14,14 +14,14 @@ categoryTest.push('categoryTest')
 var testItem0 = {
     name: 'puyo puyo',
     description: 'puyo puyo',
-    price: 2000,
+    price: 2000.00,
     image: 'https://static.myfigurecollection.net/upload/items/1/1274232-1a889.jpg'
 }
 categoryTest.push(testItem0);
 var testItem1 = {
     name: 'puyo puyo puyo',
     description: 'puyo puyo',
-    price: 2000,
+    price: 2000.00,
     image: 'https://static.myfigurecollection.net/upload/items/1/1274233-c3d47.jpg'
 }
 categoryTest.push(testItem1);
@@ -46,13 +46,15 @@ function addToCatalogue(item, id, category){
 // cart functions
 function pushToCartList(category, id){
     var cartItem = {
-        name: category[id].name,
-        price: category[id].price,
+        prod_name: category[id].name,
+        prod_price: category[id].price,
     }
     cartList.push(cartItem);
+    saveCartToLocal();
     generateCart();
 }
 function generateCart(){
+    parseLocalCart();
     if (cartList.length>1){
         document.getElementById('shopping-cart').innerHTML = '';
         cartList[0] = 0;
@@ -61,22 +63,26 @@ function generateCart(){
             cartList[0] += cartList[count].price;
             addToCart(cartItem, count);
         }
-        document.getElementById('cart-total').innerHTML = '<h6 class="mt-2">Total: ₱'+cartList[0]+' </h6>'
+        document.getElementById('cart-total').innerHTML = 'Total: ₱'+cartList[0];
     } else if(cartList.length==1){
         document.getElementById('shopping-cart').innerHTML = '<ul id="shopping-cart" class="list-group list-group-flush"> <li class="list-group-item cart-item"> <span class="mt-2 gray-out" style="font-weight: bold;">Shopping item goes here.</span> <br> <span class="mb-2 gray-out">Price goes here.</span> <span class="mb-2 gray-out">Remove</span> </li> </ul>';
         document.getElementById('cart-total').innerHTML = '<h6 class="mt-2">Total: </h6>'
     }
-    saveCartToLocal();
 }
 function addToCart(item, id){
-    document.getElementById('shopping-cart').innerHTML += '<li class="list-group-item cart-item"  id="cart' + id + '"> <span class="mt-2" style="font-weight: bold;">' + item.name + '</span> <br> <span class="mb-2">₱' + item.price + '</span> <span class="mb-2" onclick="removeFromCart(&apos;' + id + '&apos;)" style="color: firebrick; cursor: pointer;">Remove</span> </li>'
+    document.getElementById('shopping-cart').innerHTML += '<li class="list-group-item cart-item"  id="cart' + id + '"> <span class="mt-2" style="font-weight: bold;">' + item.name + '</span> <br> <span class="mb-2">₱' + item.price + '</span> <span class="mb-2" onclick="removeFromCartList(&apos;' + id + '&apos;)" style="color: firebrick; cursor: pointer;">Remove</span> </li>'
 }
-function removeFromCart(id){
+function removeFromCartList(id){
     cartList.splice(id,1);
+    saveCartToLocal();
     generateCart();
 }
 function checkOut(){
     window.location.href = 'order.html';
+}
+function inputCart(){
+    parseLocalCart()
+    document.getElementById('cart-input').value=JSON.stringify(cartList);
 }
 // local storage functions
 function saveCartToLocal(){
@@ -89,9 +95,3 @@ function parseLocalCart(){
         cartList = local_cartList;
     }
 }
-
-// commands at start //
-generateCatalogue(categoryTest); // in the final site, this would be generateGeneralCatalogue() to load all categories
-parseLocalCart();
-generateCart();
-document.getElementById('categAll').checked = true;
